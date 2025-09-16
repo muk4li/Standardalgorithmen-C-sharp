058ae9dc2bae297e270fcf0ad9b29965a57c1427
ITA4-TN06(h.b.) <huutrong1997@web.de>
2025-09-16 15:09:40 +0200

    mit Verweis...hinzufügen den namespace "Anzeigen" mit der Klasse "Anzeigend" in z.b. binary Search ansprechbar machen.

---
+++ 00Fundament/Anzeigen/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anzeigen
+{
+    public static class Anzeigend
+    {
+
+        static void Main(string[] args)
+        {
+        }
+
+        public static void StringArrayAnzeigen(string[] StringArray)
+        {
+            for (int i = 0; i < StringArray.Length; i++)
+            { 
+                Console.WriteLine(StringArray[i]);
+            }
+
+        }
+
+
+
+
+    }
+}
