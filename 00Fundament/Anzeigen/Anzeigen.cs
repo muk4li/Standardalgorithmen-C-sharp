e0d06581b92b039066dddef93f79b95248f6e9e0
ITA4-TN06(h.b.) <huutrong1997@web.de>
2025-09-17 09:33:16 +0200

    kurz vor Klausur. MergeSort hinzugefügt. Kommentierungen bei ClAnzeigen erstellt.

--- 00Fundament/Anzeigen/Anzeigen.cs
+++ 00Fundament/Anzeigen/Anzeigen.cs
@@ -9,7 +9,7 @@ namespace NSAnzeigen
     
     public static class ClAnzeigen
     {
-        static Random zufall = new Random();
+        public static Random zufall = new Random();
 
         static void Main(string[] args)
         {
@@ -20,7 +20,11 @@ namespace NSAnzeigen
 
 
 
-
+        /// <summary>
+        /// returns an randomized int array with the Length as the given size (int)
+        /// </summary>
+        /// <param name="size"></param>
+        /// <returns>int array (with given size, but randomized elements range of -128 to +127)</returns>
         public static int[] FillRandomIntArraySizeOf(int size)
         {
             int[] randomIntArray = new int[size];
@@ -32,10 +36,18 @@ namespace NSAnzeigen
 
             return randomIntArray;
         }
+        /// <summary>
+        /// Console.Writelines each element from given int array. As for-loop.
+        /// </summary>
+        /// <param name="intArray"></param>
         public static void PrintIntArray(int[] intArray)
         {
             for (int i = 0; i < intArray.Length; i++) { Console.WriteLine(intArray[i]); }
         }
+        /// <summary>
+        /// Console.Writelines each element from given int array. As for-loop.
+        /// </summary>
+        /// <param name="stringArray"></param>
         public static void PrintStringArray(string[] stringArray)
         {
             for (int i = 0; i < stringArray.Length; i++)
---
+++ 01Sortierung/MergeSort/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSAnzeigen;
+using NS_Standardalgorithmen_Csharp;
+
+namespace MergeSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = ClAnzeigen.FillRandomIntArraySizeOf(10);
+            Console.WriteLine("unsortiert:");
+            ClAnzeigen.PrintIntArray(array);
+
+            MergeSort(array, 0, array.Length-1);
+            Console.WriteLine("sortiert");
+            ClAnzeigen.PrintIntArray(array);
+
+
+
+        }
+
+        static void MergeSort(int[] intArray,int firstPosition,int lastPosition)
+        {
+            if (firstPosition < lastPosition)
+            {
+                int midIndex = (lastPosition + firstPosition) / 2;
+                MergeSort(intArray, firstPosition, midIndex);
+                MergeSort(intArray, midIndex + 1, lastPosition);
+
+
+                Merge(intArray, firstPosition, midIndex, lastPosition);
+            }
+        }
+        static void Merge(int[] intArray, int min, int mid, int max)
+        {
+            int i1 = mid - min+1;
+            int i2 = max - mid;
+
+            int[] leftTree = new int[i1];
+            int[] rightTree = new int[i2];
+
+            for (int i =0; i<i1;i++)
+                leftTree[i] = intArray[min+i];
+            for (int j = 0; j < i2; j++)
+                rightTree[j] = intArray[mid + 1 + j];
+
+            int leftI = 0, rightI = 0, index = min;
+
+            while (leftI < i1 && rightI < i2)
+            {
+                if (leftTree[leftI] <= rightTree[rightI])
+                {
+                    intArray[index] = leftTree[leftI];
+                    leftI++;
+                }
+                else
+                {
+                    intArray[index] = rightTree[rightI];
+                    rightI++;
+                }
+                index++;
+            }
+
+            while (leftI < i1)
+            {
+                intArray[index] = leftTree[(leftI++)];
+                index++;
+            }
+
+            while (rightI < i2)
+            {
+                intArray[index] = rightTree[(rightI++)];
+                index++;
+            }
+        }
+
+
+
+    }
+}
